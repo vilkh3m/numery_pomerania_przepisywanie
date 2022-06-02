ff556551bfc926388cf9c88c04b93c621cd038ba
Dominik Pietrzak <vilkh3m@gmail.com>
2022-06-02 21:30:16 +0200

    Event 64

---
+++ skrypt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#prod
+ARTI=/var/www/html/mtb/artisan
+
+#test
+#ARTI=/home/dominik/kopia/mtb/artisan
+
+#PHP path
+PHPPATH=/usr/bin/php
+
+TARGET_EVENT=64
+TARGET_MEGA=136
+TARGET_SPEED=137
+
+# event 63
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 63 134 $TARGET_EVENT $TARGET_MEGA
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 63 135 $TARGET_EVENT $TARGET_SPEED
+
+# event 62
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 62 132 $TARGET_EVENT $TARGET_MEGA
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 62 133 $TARGET_EVENT $TARGET_SPEED
+
+# event 61
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 61 130 $TARGET_EVENT $TARGET_MEGA
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 61 131 $TARGET_EVENT $TARGET_SPEED
+
+# event 60
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 60 127 $TARGET_EVENT $TARGET_MEGA
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 60 129 $TARGET_EVENT $TARGET_SPEED
+
+# event 59
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 59 125 $TARGET_EVENT $TARGET_MEGA
+$PHPPATH $ARTI user:reassignusertrackstartnumbers 59 126 $TARGET_EVENT $TARGET_SPEED
